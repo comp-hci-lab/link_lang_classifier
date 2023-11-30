81b9f01d3f6cb1bf9600eb586fd81f76e1efec70
Nel Escher <kescher@umich.edu>
2023-11-30 16:20:36 -0500

    update origins for websocket

--- lib/link_lang_classifier_web/endpoint.ex
+++ lib/link_lang_classifier_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule LinkLangClassifierWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options], check_origin: ["https://lang-labeling.eecs.umich.edu", "//lang-labeling.eecs.umich.edu", "//*.umich.edu", "http://localhost:4000"]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
